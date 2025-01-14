98bd3b0ecb3e237769f00f0beb3f4d9c54fda338
Sergey Lvov <Lvov.Sergey.V@yandex.ru>
2025-01-14 15:13:40 +0300

    Поправил test_question добавил в него параметизацию, поправил тесты с логотипом Яндекс и Самокат, убрал ссылки под переменные

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ from selenium import webdriver
 class StoreConfig:
     URL_YA_SCOOTER = "https://qa-scooter.praktikum-services.ru/" #Ссылка на приложение магазина "Яндекс Самокат"
     URL_DZEN = "https://dzen.ru/?yredirect=true" #Ссылка на страницу дзен, нужна для проверки нахождения
+    URL_YA_SCOOTER_ORDER = "https://qa-scooter.praktikum-services.ru/order" # Ссылка на страницу заказа Самоката
     TIMEOUT = 10 # Тайм-аут ожидания элемента страницы в секундах
--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,4 +1,5 @@
 import allure
+import pytest
 
 from config import StoreConfig as Sc
 from conftest import setup_class, teardown_class
@@ -15,9 +16,22 @@ class TestQuestion:
         # создали драйвер для браузера
         cls.driver = setup_class()
 
-    @allure.title('Проверки раскрытия ответа на вопрос "Сколько это стоит? И как оплатить?')
+    @pytest.mark.parametrize(
+        'question, answer, title_question',
+        [
+            [Ql.QUESTION_PRICE, Ql.ANSWER_PRICE, "Сколько это стоит? И как оплатить?"],
+            [Ql.QUESTION_RENT_MULTIPLE_SCOOTERS, Ql.ANSWER_RENT_MULTIPLE_SCOOTERS, "Хочу сразу несколько самокатов! Так можно?"],
+            [Ql.QUESTION_RENT_TIME_CALCULATED, Ql.ANSWER_RENT_TIME_CALCULATED, "Как рассчитывается время аренды?"],
+            [Ql.QUESTION_ORDER_SCOOTER_TODAY, Ql.ANSWER_ORDER_SCOOTER_TODAY, "Можно ли заказать самокат прямо на сегодня?"],
+            [Ql.QUESTION_EXTEND_ORDER, Ql.ANSWER_EXTEND_ORDER, "Можно ли продлить заказ или вернуть самокат раньше?"],
+            [Ql.QUESTION_DELIVER_CHARGER, Ql.ANSWER_DELIVER_CHARGER, "Вы привозите зарядку вместе с самокатом?"],
+            [Ql.QUESTION_CANCEL_ORDER, Ql.ANSWER_CANCEL_ORDER, "Можно ли отменить заказ?"],
+            [Ql.QUESTION_DELIVER_OUTSIDE_MKAD, Ql.ANSWER_DELIVER_OUTSIDE_MKAD, "Я живу за МКАДом, привезёте?"]
+        ]
+    )
+    @allure.title('Проверки раскрытия ответа на вопрос "{title_question}"')
     @allure.description('Нажимаем на вопрос и проверяем видимость ответа, элемента accordion__panel')
-    def test_question_price(self): #Проверки раскрытия ответа на вопрос "Сколько это стоит? И как оплатить?"
+    def test_question(self, question,answer, title_question): #Проверки раскрытия ответа на вопрос
         #Открытие страницы магазина
         self.driver.get(Sc.URL_YA_SCOOTER)
 
@@ -27,139 +41,11 @@ class TestQuestion:
         #Скроллим страницу до последнего элемента
         base_page.scroll_to_element(Ql.QUESTION_DELIVER_OUTSIDE_MKAD)
 
-        # Нажатие на раскрывающийся список "Сколько это стоит? И как оплатить?"
-        base_page.click_element(Ql.QUESTION_PRICE)
+        # Нажатие на раскрывающийся список
+        base_page.click_element(question)
 
-        #Проверяем видимость ответа на вопрос "Сколько это стоит? И как оплатить?"
-        assert base_page.check_is_displayed(Ql.ANSWER_PRICE)
-
-    @allure.title('Проверки раскрытия ответа на вопрос "Проверки раскрытия ответа на вопрос "Хочу сразу несколько самокатов! Так можно?')
-    @allure.description('Нажимаем на вопрос и проверяем видимость ответа, элемента accordion__panel')
-    def test_question_rent_multiple_scooters(self): #Проверки раскрытия ответа на вопрос "Хочу сразу несколько самокатов! Так можно?"
-        #Открытие страницы магазина
-        self.driver.get(Sc.URL_YA_SCOOTER)
-
-        #Создаем объект класса базовой страницы
-        base_page = BasePage(self.driver)
-
-        #Скроллим страницу до последнего элемента
-        base_page.scroll_to_element(Ql.QUESTION_DELIVER_OUTSIDE_MKAD)
-
-
-        # Нажатие на раскрывающийся список "Хочу сразу несколько самокатов! Так можно?"
-        base_page.click_element(Ql.QUESTION_RENT_MULTIPLE_SCOOTERS)
-
-        #Проверяем видимость ответа на вопрос "Хочу сразу несколько самокатов! Так можно?"
-        assert base_page.check_is_displayed(Ql.ANSWER_RENT_MULTIPLE_SCOOTERS)
-
-    @allure.title('#Проверки раскрытия ответа на вопрос "Как рассчитывается время аренды?"')
-    @allure.description('Нажимаем на вопрос и проверяем видимость ответа, элемента accordion__panel')
-    def test_question_time_calculated(self): #Проверки раскрытия ответа на вопрос "Как рассчитывается время аренды?"
-        #Открытие страницы магазина
-        self.driver.get(Sc.URL_YA_SCOOTER)
-
-        #Создаем объект класса базовой страницы
-        base_page = BasePage(self.driver)
-
-        #Скроллим страницу до последнего элемента
-        base_page.scroll_to_element(Ql.QUESTION_DELIVER_OUTSIDE_MKAD)
-
-
-        # Нажатие на раскрывающийся список "Как рассчитывается время аренды?"
-        base_page.click_element(Ql.QUESTION_RENT_TIME_CALCULATED)
-
-        #Проверяем видимость ответа на вопрос "Как рассчитывается время аренды?"
-        assert base_page.check_is_displayed(Ql.ANSWER_RENT_TIME_CALCULATED)
-
-    @allure.title('Проверки раскрытия ответа на вопрос "Можно ли заказать самокат прямо на сегодня?"')
-    @allure.description('Нажимаем на вопрос и проверяем видимость ответа, элемента accordion__panel')
-    def test_question_order_scooter_today(self): #Проверки раскрытия ответа на вопрос "Можно ли заказать самокат прямо на сегодня?"
-        # Открытие страницы магазина
-        self.driver.get(Sc.URL_YA_SCOOTER)
-
-        # Создаем объект класса базовой страницы
-        base_page = BasePage(self.driver)
-
-        # Скроллим страницу до последнего элемента
-        base_page.scroll_to_element(Ql.QUESTION_DELIVER_OUTSIDE_MKAD)
-
-        # Нажатие на раскрывающийся список "Можно ли заказать самокат прямо на сегодня?"
-        base_page.click_element(Ql.QUESTION_ORDER_SCOOTER_TODAY)
-
-        # Проверяем видимость ответа на вопрос "Можно ли заказать самокат прямо на сегодня?"
-        assert base_page.check_is_displayed(Ql.ANSWER_ORDER_SCOOTER_TODAY)
-
-    @allure.title('Проверки раскрытия ответа на вопрос "Можно ли продлить заказ или вернуть самокат раньше?"')
-    @allure.description('Нажимаем на вопрос и проверяем видимость ответа, элемента accordion__panel')
-    def test_question_extend_order(self): #Проверки раскрытия ответа на вопрос "Можно ли продлить заказ или вернуть самокат раньше?"
-        # Открытие страницы магазина
-        self.driver.get(Sc.URL_YA_SCOOTER)
-
-        # Создаем объект класса базовой страницы
-        base_page = BasePage(self.driver)
-
-        # Скроллим страницу до последнего элемента
-        base_page.scroll_to_element(Ql.QUESTION_DELIVER_OUTSIDE_MKAD)
-
-        # Нажатие на раскрывающийся список "Можно ли продлить заказ или вернуть самокат раньше?"
-        base_page.click_element(Ql.QUESTION_EXTEND_ORDER)
-
-        # Проверяем видимость ответа на вопрос "Можно ли продлить заказ или вернуть самокат раньше?"
-        assert base_page.check_is_displayed(Ql.ANSWER_EXTEND_ORDER)
-
-    @allure.title('Проверки раскрытия ответа на вопрос "Вы привозите зарядку вместе с самокатом?"')
-    @allure.description('Нажимаем на вопрос и проверяем видимость ответа, элемента accordion__panel')
-    def test_question_deliver_charger(self): #Проверки раскрытия ответа на вопрос "Вы привозите зарядку вместе с самокатом?"
-        # Открытие страницы магазина
-        self.driver.get(Sc.URL_YA_SCOOTER)
-
-        # Создаем объект класса базовой страницы
-        base_page = BasePage(self.driver)
-
-        # Скроллим страницу до последнего элемента
-        base_page.scroll_to_element(Ql.QUESTION_DELIVER_OUTSIDE_MKAD)
-
-        # Нажатие на раскрывающийся список "Вы привозите зарядку вместе с самокатом?"
-        base_page.click_element(Ql.QUESTION_DELIVER_CHARGER)
-
-        # Проверяем видимость ответа на вопрос "Вы привозите зарядку вместе с самокатом?"
-        assert base_page.check_is_displayed(Ql.ANSWER_DELIVER_CHARGER)
-
-    @allure.title('Проверки раскрытия ответа на вопрос "Можно ли отменить заказ?"')
-    @allure.description('Нажимаем на вопрос и проверяем видимость ответа, элемента accordion__panel')
-    def test_question_answer_cancel_order(self): #Проверки раскрытия ответа на вопрос "Можно ли отменить заказ?"
-        # Открытие страницы магазина
-        self.driver.get(Sc.URL_YA_SCOOTER)
-
-        # Создаем объект класса базовой страницы
-        base_page = BasePage(self.driver)
-
-        # Скроллим страницу до последнего элемента
-        base_page.scroll_to_element(Ql.QUESTION_DELIVER_OUTSIDE_MKAD)
-
-        # Нажатие на раскрывающийся список "Можно ли отменить заказ?"
-        base_page.click_element(Ql.QUESTION_CANCEL_ORDER)
-
-        # Проверяем видимость ответа на вопрос "Можно ли отменить заказ?"
-        assert base_page.check_is_displayed(Ql.ANSWER_CANCEL_ORDER)
-
-    @allure.title('Проверки раскрытия ответа на вопрос "Я живу за МКАДом, привезёте?"')
-    @allure.description('Нажимаем на вопрос и проверяем видимость ответа, элемента accordion__panel')
-    def test_question_deliver_outside_mkad(self): #Проверки раскрытия ответа на вопрос "Я живу за МКАДом, привезёте?"
-        # Открытие страницы магазина
-        self.driver.get(Sc.URL_YA_SCOOTER)
-
-        # Создаем объект класса базовой страницы
-        base_page = BasePage(self.driver)
-
-        # Скроллим страницу до последнего элемента
-        base_page.scroll_to_element(Ql.QUESTION_DELIVER_OUTSIDE_MKAD)
-
-        # Нажатие на раскрывающийся список "Я живу за МКАДом, привезёте?"
-        base_page.click_element(Ql.QUESTION_DELIVER_OUTSIDE_MKAD)
-
-        # Проверяем видимость ответа на вопрос "Я живу за МКАДом, привезёте?"
-        assert base_page.check_is_displayed(Ql.ANSWER_DELIVER_OUTSIDE_MKAD)
+        #Проверяем видимость ответа на вопрос
+        assert base_page.check_is_displayed(answer), f'Ответ на вопрос "{title_question}" не отображается на странице'
 
     @classmethod
     def teardown_class(cls):
@@ -186,8 +72,8 @@ class TestButtonOrder:
         #Нажимаем на кнопку "Заказать"
         base_page.click_element(Hl.BUTTON_HEADER_ORDER)
 
-        #Проверяем нахождение на страницы "Заказа" https://qa-scooter.praktikum-services.ru/order
-        assert base_page.check_current_url("https://qa-scooter.praktikum-services.ru/order", Of.BUTTON_NEXT)
+        #Проверяем нахождение на страницы "Заказа"
+        assert base_page.check_current_url(Sc.URL_YA_SCOOTER_ORDER, Of.BUTTON_NEXT)
 
     @allure.title('Проверка кнопки "Заказать" на домашней страницы')
     @allure.description('Нажимаем на кнопку "Заказать" на домашней странице, ждем видимости кнопку "Далее"  и проверяем фактическую страницу с ожидаемой')
@@ -204,8 +90,8 @@ class TestButtonOrder:
         #Нажимаем на кнопку "Заказать"
         base_page.click_element(Hrm.BUTTON_HOME_ORDER)
 
-        #Проверяем нахождение на страницы "Заказа" https://qa-scooter.praktikum-services.ru/order
-        assert base_page.check_current_url("https://qa-scooter.praktikum-services.ru/order", Of.BUTTON_NEXT)
+        #Проверяем нахождение на страницы "Заказа"
+        assert base_page.check_current_url(Sc.URL_YA_SCOOTER_ORDER, Of.BUTTON_NEXT)
 
 
 
